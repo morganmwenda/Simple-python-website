12a7b3c258d703f9e72578e14ccbafc0cff2aa51
Morgan <160935122+morganmwenda@users.noreply.github.com>
2024-04-11 14:50:45 +0000

    new edit

--- Website/auth.py
+++ Website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint #shows the blueprint of the website
 
 auth = Blueprint('auth',__name__)
 
-#@auth.route('/')  # put the url for the end point here
+@auth.route('/login')  # put the url for the end point here
 
-#def login():
- #   return "<h1> test </h1>"
+def login():
+    return "<h1> test </h1>"
