1c5402568554a9f2d92d1d15efbe2c584bf541c3
Morgan <160935122+morganmwenda@users.noreply.github.com>
2024-06-14 12:35:27 +0300

    redo

--- Website/auth.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint #shows the blueprint of the website
-
-auth = Blueprint('auth',__name__)
-
-@auth.route('/login')  # put the url for the end point here
-
-def login():
-    return "<h1> test </h1>"
--- Website/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint #shows the blueprint of the website
-
-views = Blueprint('views',__name__)
-
-@views.route('/')  # put the url for the end point here
-
-def home():
-    return "<h1> test </h1>"
